91bf4752bb60ffdd4f7427e71bb2d40a37e3dc98
Uran_Hajrizaj <uranhajrizaj@outlook.com>
2022-07-20 16:45:00 +0200

    changed

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ let state = {
     id: "",
     password: "",
     type: "",
-  }
+  },
+  alertMessage: "",
 };
 
 function getStudentsfromServer() {
@@ -33,7 +34,7 @@ getProfesorsfromServer();
 
 let bodyEl = document.querySelector("body");
 
-function signIn(email, password) {
+function signIn(email:String, password:String, divEl:HTMLDivElement) {
   fetch(`http://localhost:3005/professors/${email}`)
     .then((resp) => resp.json())
     .then((professor) => {
@@ -52,7 +53,8 @@ function signIn(email, password) {
               state.user.type = "student";
               render()
             } else {
-              alert("Invalid email or password.");
+              state.alertMessage = "Wrong email or password. Please try again";
+              render()
             }
           });
       }
@@ -67,15 +69,20 @@ function renderHomePage() {
   let holderEl = document.createElement("div");
   holderEl.className = "holder";
 
+  let titleEl = document.createElement("h1");
+  titleEl.textContent = "Welcome! Please sign in!";
+  titleEl.className = "home-title";
+  containerEl.append(titleEl);
+
   let h1El = document.createElement("h1");
-  h1El.textContent = "ESSM";
+  h1El.textContent = "essm";
   h1El.className = "title";
 
   let formEl = document.createElement("form");
   formEl.className = "form";
   formEl.addEventListener("submit", function (event) {
     event.preventDefault();
-    signIn(userEl.value, passwordEl.value);
+    signIn(userEl.value, passwordEl.value, divEl);
     render();
   });
 
@@ -84,6 +91,10 @@ function renderHomePage() {
   userEl.placeholder = "Enter your email";
   userEl.className = "user";
   userEl.required = true;
+  
+  let divEl = document.createElement("div");
+  divEl.className = "div-password";
+  
 
   let passwordEl = document.createElement("input");
   passwordEl.type = "password";
@@ -91,11 +102,21 @@ function renderHomePage() {
   passwordEl.className = "password";
   passwordEl.required = true;
 
+  divEl.append(passwordEl);
+
+  if(state.alertMessage !== ""){
+    let pEl = document.createElement("p");
+              pEl.textContent = "* Wrong email or password. Please try again!";
+              pEl.className="alert-message";
+              divEl.append(pEl);
+  }
+
   let buttonEl = document.createElement("button");
   buttonEl.textContent = "Log in";
   buttonEl.className = "login-button";
-
-  formEl.append(userEl, passwordEl, buttonEl);
+  
+ 
+  formEl.append(userEl, divEl, buttonEl);
   holderEl.append(h1El, formEl);
   containerEl.append(holderEl);
   bodyEl.append(containerEl);
@@ -104,13 +125,44 @@ function renderHomePage() {
 function renderProfessorPage() {
   if (bodyEl === null) return;
   bodyEl.textContent = "";
+   
   let containerEl = document.createElement("div");
-  containerEl.className = "container";
-  let pEl = document.createElement("h1");
-  pEl.textContent = "Welcome professor";
+  containerEl.className = "container-professor";
 
-  containerEl.append(pEl);
-  bodyEl.append(containerEl);
+    let headerEl = document.createElement("header");
+    headerEl.className = "header";
+  
+    let hollixtionEl = document.createElement("a");
+
+    let h1El = document.createElement("h1");
+    h1El.textContent = "HOLLIXTON";
+    hollixtionEl.append(h1El);
+  
+    let grilsLinkEl = document.createElement("a");
+    grilsLinkEl.textContent = "Girls";
+   
+    let guysLinkEl = document.createElement("a");
+    guysLinkEl.textContent = "Guys";
+  
+    let saleLinkEl = document.createElement("a");
+    saleLinkEl.textContent = "Sale";
+    saleLinkEl.addEventListener("click", function () {
+ 
+    let linksEl = document.createElement("div");
+    linksEl.className = "links";
+  
+    let buttonsEl = document.createElement("div");
+    buttonsEl.className = "buttons";
+  
+    let searchButtonEl = document.createElement("button");
+    searchButtonEl.className = "material-symbols-outlined";
+    searchButtonEl.textContent = "search";
+
+ 
+})
+    headerEl.append(hollixtionEl, grilsLinkEl, guysLinkEl, saleLinkEl);
+    containerEl.append(headerEl);
+    bodyEl.append(containerEl);
 }
 
 function renderStudentPage() {
